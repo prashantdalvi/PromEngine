dfc0685db84824f7e3e12b5e9d5a79fb9295d4d9
prashantdalvi <61616275+prashantdalvi@users.noreply.github.com>
2020-08-27 16:34:59 +0530

    added DAL

---
+++ PromotionEngineDataAccess/JsonParser.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using PromotionEngine.Model;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PromotionEngine.DataAccess
+{
+    public static class JsonParser
+    {
+        public static void JsonParserToList(string filePath, PromotionData promotionData)
+        {
+            try
+            {
+                using (StreamReader r = new StreamReader(filePath))
+                {
+                    string json = r.ReadToEnd();
+                    promotionData = Newtonsoft.Json.JsonConvert.DeserializeObject<PromotionData>(json);
+                    Global.activePromotions = promotionData.ActivePromotion.ToList();
+                    Global.skuIdPrice = promotionData.SkuIdPrice.ToList();
+                }
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        public static List<SkuIdPrice> JsonParserToActivePromotion(string filePath)
+        {
+            List<SkuIdPrice> skuIdPrice = new List<SkuIdPrice>();
+            try
+            {
+                using (StreamReader r = new StreamReader(filePath))
+                {
+                    string json = r.ReadToEnd();
+                    skuIdPrice = JsonConvert.DeserializeObject<List<SkuIdPrice>>(json);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+
+            return skuIdPrice;
+        }
+    }
+}
