aa055750970158f0550686bce0f2a93d57b9c57a
prashantdalvi <61616275+prashantdalvi@users.noreply.github.com>
2020-09-02 20:58:27 +0530

    Add files via upload

---
+++ PromotionEngine.Repository/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using PromotionEngine.Repository.IRepository;
+
+namespace Uplift.DataAccess.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        public Repository()
+        {
+            
+        }
+
+
+        public T Get(int id)
+        {
+            T var = (T)(object)42;
+            return default(T);
+        }
+    }
+}
